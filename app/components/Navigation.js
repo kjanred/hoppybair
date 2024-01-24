6e6099882e226e6b93ac2a6611569adfa27a9251
kjanred <janredkarl@gmail.com>
2024-01-24 15:53:43 -0800

    created a second page. created working Links in the navigation.

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,28 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { tarrgetFont } from '../font/font';
 
 export default function Navigation() {
     return (
         <nav className={`${tarrgetFont.className} navbar navbar-expand-lg bg-body-tertiar sticky-top`}>
         <div className="container">
-                    <a className="navbar-brand" href="#">
+                    <Link className="navbar-brand" href="/">
                       <Image
                         src='/img/logo.png'
                         width={100}
                         height={100}
                         alt="logo"
                       />
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul className="navbar-nav">
             <li className="nav-item">
-                <a className="nav-link" href="#">Home</a>
+                <Link className="nav-link" href="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Events</a>
+                <Link className="nav-link" href="/events">Events</Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">About</a>
---
+++ app/events/page.js
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+import { tarrgetFont } from '../font/font';
+
+export default function Events() {
+
+
+  return (
+    <div className="container-fluid p-0 main-container">
+
+    
+      <Navigation />
+    
+    <div className="row p-2">
+        <div className="col-md-2"><br /></div>
+              <div id="introh1" className="col-md-8 my-3 my-md-0 p-2 text-center homeCard"><h1 className={`${tarrgetFont.className} text-start ps-2 introH1`}>Next Event: </h1>
+              <p className='nextEvent'>Our next event is: <span className="fs-1">01/03/23 6:00pm</span><br />at <span className="fs-1">Hen House Santa Rosa</span></p> 
+       
+        </div>
+        <div className="col-md-2"><br /></div>
+    </div>
+      
+    <div className="row events p-2">
+        <div className="col-md-2"><br /></div>
+
+        <div className="col-md-4 p-5 my-3 my-md-0 homeCard d-flex justify-content-center align-items-center"><p className='nextEvent'>Our next event is: <span className="fs-1">01/03/23 6:00pm</span><br />at <span className="fs-1">Hen House Santa Rosa</span></p> </div>
+        <div className="col-md-4 p-5 my-3 my-md-0 homeCard d-flex justify-content-center align-items-center">
+          <Image
+            src="/img/henhouse.png"
+            height={200}
+            width={300}
+            alt="henhouse logo"
+          /> </div>
+        <div className="col-md-2"><br /></div>
+    </div>
+    
+        <Footer />
+    
+</div>
+  )
+}
+
+
